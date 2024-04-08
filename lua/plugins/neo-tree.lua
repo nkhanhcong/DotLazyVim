44e0a8b3ca3105be277c3dd0fe513281573d9ab3
nkhanhcong <nkhanhcong@gmail.com>
2024-04-08 07:55:26 +0700

    fix some conflict

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -6,8 +6,5 @@ return {
     "nvim-tree/nvim-web-devicons",
     "MunifTanjim/nui.nvim",
   },
-  config = function()
-    vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>", {})
-    vim.keymap.set("n", "<leader>hh", ":Neotree buffers reveal float<CR>", {})
-  end,
+  config = function() end,
 }
