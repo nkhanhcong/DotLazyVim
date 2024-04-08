6ea74d619a2cf0ef82058cba9427aea5ed704e2a
nkhanhcong <nkhanhcong@gmail.com>
2024-04-08 07:46:56 +0700

    add config mason

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -2,19 +2,53 @@ return {
   "williamboman/mason.nvim",
   dependencies = {
     "williamboman/mason-lspconfig.nvim",
-    "jayp0521/mason-null-ls.nvim",
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
   },
   config = function()
+    -- import mason
     local mason = require("mason")
+
+    -- import mason-lspconfig
     local mason_lspconfig = require("mason-lspconfig")
-    local mason_null_ls = require("mason-null-ls")
+
+    local mason_tool_installer = require("mason-tool-installer")
 
     -- enable mason and configure icons
-    mason.setup()
+    mason.setup({
+      ui = {
+        icons = {
+          package_installed = "✓",
+          package_pending = "➜",
+          package_uninstalled = "✗",
+        },
+      },
+    })
 
     mason_lspconfig.setup({
-      -- auto-install configured servers (with lspconfig)
-      automatic_installation = true,
+      -- list of servers for mason to install
+      ensure_installed = {
+        "tsserver",
+        "html",
+        "cssls",
+        "tailwindcss",
+        "svelte",
+        "lua_ls",
+        "graphql",
+        "emmet_ls",
+        "prismals",
+        "pyright",
+      },
+    })
+
+    mason_tool_installer.setup({
+      ensure_installed = {
+        "prettier", -- prettier formatter
+        "stylua", -- lua formatter
+        "isort", -- python formatter
+        "black", -- python formatter
+        "pylint",
+        "eslint_d",
+      },
     })
   end,
 }
